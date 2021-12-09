a51b1562e6cbc9858b5cf2820b5774e590a90eda
jhaturaphat <umt.eng52@gmail.com>
2021-12-09 23:16:28 +0700

    hone code add route type

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -13,6 +13,10 @@ router.delete('/group/:id',(req, res) => res.sendAsyncApi(Group.delete(req.param
 
 const Type = new TypeController();
 router.get('/type', (req, res) => res.sendAsyncApi(Type.findAll()));
+router.get('/type/:id',(req, res) => res.sendAsyncApi(Group.findOne(req.params.id)));
+router.post('/type',(req, res) => res.sendAsyncApi(Group.save(req.body)));
+router.put('/type/:id',(req, res) => res.sendAsyncApi(Group.update(req.params.id,req.body)));
+router.delete('/type/:id',(req, res) => res.sendAsyncApi(Group.delete(req.params.id)));
 
 const Name = new NameController();
 router.get('/name', (req, res) => res.sendAsyncApi(Name.findAll()));
