7a84dbe58a6f6b1d9038fbc36fe5ca3c8bdc3363
umt.eng52@gmail.com <umt.eng52@gmail.com>
2022-03-04 10:29:23 +0700

    add env his

---
+++ backend/src/configs/HisDatabase.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const mysql = require('mysql'); // เรียกใช้งาน MySQL module
+
+class HisDatabase {
+
+    constructor() {
+        this.connection = mysql.createPool({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_DATABASES,
+            charset: process.env.DB_CHARSET,
+            port:process.env.DB_PORT
+        });
+    }
+
+    // Custom ฟังชั่นก์ Query ข้อมูลใหม่
+    query(sql, params = null) {
+        return new Promise((resolve, reject) => {           
+            this.connection.query(sql, params, (errors, result) => {
+                if (errors) return reject({ errors });                
+                resolve(result);
+            });
+        });
+    }
+} 
+
+module.exports = {HisDatabase}
--- backend/src/middleware/authorization-middleware.js
+++ backend/src/middleware/authorization-middleware.js
@@ -1,8 +1,34 @@
 const jwt = require("jsonwebtoken")
 const config = require("../configs/jwt_secret")
 
-module.exports = ()=>{
- 
+module.exports = (credentials = [])=>{
+    return (req, res, next) => {
+        if(typeof credentials === "string"){
+            credentials = [credentials]
+        }
+
+        //มองหา JWT in Header
+        const token = req.header['authorization'];
+        if(!token) return res.status(401).send("ไม่มีสิทธิ์เข้าถึง: access denied");
+        // ตรวจสอบ JWT Bearer 
+        const tokenBody = token.slice(7);
+        jwt.verify(tokenBody, config.JWT_SECRET, (err, decoded)=>{
+            if(err) return res.status(401).send("Error: Access Denied");
+            // ไม่มีข้อผิดพลาด JWT ดี!
+            // ตรวจสอบข้อมูลประจำตัวที่ถูกส่งผ่านใน
+            if(credentials.length > 0){
+                if(decoded.scope && jwt.decoded.scope.length && credentials.some(cred => decoded.scopes.indexOf(cred) >= 0)){
+                    next();
+                }else{
+                    return res.status(401).send("Error: Access Denied");
+                }
+            }else{
+                // ไม่จำเป็นต้องมีข้อมูลประจำตัวผู้ใช้ได้รับอนุญาต
+                next();
+            }
+
+        });
+    }
 }
 
 // https://github.com/brian-childress/node-authorization-middleware/blob/master/authorization-middleware.js
